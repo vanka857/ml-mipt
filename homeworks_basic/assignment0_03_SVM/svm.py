88b5537ddecf91be93f047c2693b05e06c98f4dd
vankudr <vanka857@gmail.com>
2021-11-21 19:12:11 +0300

    attempt1

--- homeworks_basic/assignment0_03_SVM/svm.py
+++ homeworks_basic/assignment0_03_SVM/svm.py
@@ -17,7 +17,7 @@ def rbf(x_1, x_2, sigma=1.):
         kernel function values for all pairs of samples from x_1 and x_2
         torch.tensor of type torch.float32 shaped `(#samples_1, #samples_2)`
     '''
-    distances = ### YOUR CODE HERE
+    distances = torch.exp(-torch.cdist(x_1, x_2)**2 / (2*sigma**2))
     return torch.Tensor(distances).type(torch.float32)
 
 def hinge_loss(scores, labels):
@@ -25,7 +25,7 @@ def hinge_loss(scores, labels):
     '''
     assert len(scores.shape) == 1
     assert len(labels.shape) == 1
-    return ### YOUR CODE HERE
+    return torch.max(torch.zeros_like(scores), 1 - scores*labels).mean()### YOUR CODE HERE
 
 
 class SVM(BaseEstimator, ClassifierMixin):
@@ -40,7 +40,7 @@ class SVM(BaseEstimator, ClassifierMixin):
             kernel function values for all pairs of samples from x_1 and x_2
             torch.tensor shaped `(#samples_1, #samples_2)` of type torch.float32
         '''
-        return ### YOUR CODE HERE
+        return x_1 @ x_2.T ### YOUR CODE HERE
     
     def __init__(
         self,
@@ -83,7 +83,9 @@ class SVM(BaseEstimator, ClassifierMixin):
                 
                 optimizer.zero_grad()     # Manually zero the gradient buffers of the optimizer
                 
-                preds = ### YOUR CODE HERE # get the matrix product using SVM parameters: self.betas and self.bias
+                # get the matrix product using SVM parameters: self.betas and self.bias
+                preds = k_batch @ self.betas + self.bias ### YOUR CODE HERE 
+
                 preds = preds.flatten()
                 loss = self.lmbd * self.betas[batch_inds].T @ k_batch @ self.betas + hinge_loss(preds, y_batch)
                 loss.backward()           # Backpropagation
@@ -102,7 +104,7 @@ class SVM(BaseEstimator, ClassifierMixin):
             batch = torch.from_numpy(batch).float()
             K = self.kernel_function(batch, self.X)
             # compute the margin values for every object in the batch
-            return ### YOUR CODE HERE
+            return torch.squeeze(torch.tanh(K @ self.betas + self.bias)) ### YOUR CODE HERE
 
     def predict(self, batch):
         scores = self.predict_scores(batch)
